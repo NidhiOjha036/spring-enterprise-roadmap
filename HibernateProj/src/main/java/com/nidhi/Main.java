314bd0d87351c6a0d422c7a7e4f61436927ece23
Nidhi <ojhanidhi036@gmail.com>
2025-11-08 19:06:51 +0530

    change tablename and columnname(Alien)

--- HibernateProj/src/main/java/com/nidhi/Main.java
+++ HibernateProj/src/main/java/com/nidhi/Main.java
@@ -6,32 +6,26 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Student s1 = new Student();
-//        s1.setRollNo(103);
-//        s1.setsName("Harsh");
-//        s1.setsAge(23);
-
-
+        Alien a1 = new Alien();
+        a1.setAid(101);
+        a1.setAname("Navin");
+        a1.setTech("Java");
 
         SessionFactory sf = new Configuration()
-                .addAnnotatedClass(com.nidhi.Student.class)
+                .addAnnotatedClass(com.nidhi.Alien.class)
                 .configure()
                 .buildSessionFactory();
 
         Session session = sf.openSession();
         Transaction tx = session.beginTransaction();
-        //session.merge(s1);
-        s1 = session.find(Student.class,106);
-        session.remove(s1);
+        session.persist(a1);
         tx.commit();
         session.close();
         sf.close();
-        System.out.println(s1);
+
 
     }
 }
