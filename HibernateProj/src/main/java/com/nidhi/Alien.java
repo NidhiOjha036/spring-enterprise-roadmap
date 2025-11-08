6ad3682e3e5d8237afb060e09bc84eadfa6e5eab
Nidhi <ojhanidhi036@gmail.com>
2025-11-08 19:33:01 +0530

    embedded laptop data in alien class

--- HibernateProj/src/main/java/com/nidhi/Alien.java
+++ HibernateProj/src/main/java/com/nidhi/Alien.java
@@ -3,16 +3,13 @@ package com.nidhi;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="alien_table")
 public class Alien {
-
 
     @Id
     private int aid;
-    @Column(name="alien_name")
     private String aname;
-    @Transient
     private String tech;
+    private Laptop laptop;
 
     public int getAid() {
         return aid;
@@ -38,12 +35,21 @@ public class Alien {
         this.tech = tech;
     }
 
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
     @Override
     public String toString() {
         return "Alien{" +
                 "aid=" + aid +
                 ", aname='" + aname + '\'' +
                 ", tech='" + tech + '\'' +
+                ", laptop=" + laptop +
                 '}';
     }
 }
--- HibernateProj/src/main/java/com/nidhi/Main.java
+++ HibernateProj/src/main/java/com/nidhi/Main.java
@@ -9,10 +9,16 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
 
+        Laptop l1 = new Laptop();
+        l1.setBrand("Asus");
+        l1.setModel("Rog");
+        l1.setRam(16);
+
         Alien a1 = new Alien();
         a1.setAid(101);
         a1.setAname("Navin");
         a1.setTech("Java");
+        a1.setLaptop(l1);
 
         SessionFactory sf = new Configuration()
                 .addAnnotatedClass(com.nidhi.Alien.class)
