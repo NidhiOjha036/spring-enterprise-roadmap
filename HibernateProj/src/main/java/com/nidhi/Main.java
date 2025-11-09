0a7676fe3fad60ccda55b37912b0042e2692d538
Nidhi <ojhanidhi036@gmail.com>
2025-11-09 23:16:48 +0530

    Get vs load

--- HibernateProj/src/main/java/com/nidhi/Main.java
+++ HibernateProj/src/main/java/com/nidhi/Main.java
@@ -1,11 +1,9 @@
 package com.nidhi;
 
-import jakarta.persistence.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.util.List;
 
 
 public class Main {
@@ -19,35 +17,13 @@ public class Main {
 
         Session session = sf.openSession();
 
-        //   Select * from laptop where ram=32 ->SQL
-        //   from Laptop where ram=32 -> HQL
+//        Laptop laptop = session.find(Laptop.class, 2);
+//        Laptop laptop = session.getReference(Laptop.class, 2);
 
-        //Query query = session.createQuery("from Laptop");
-
-        String brand = "Asus";
-
-//        Query query = session.createQuery("from Laptop where brand like 'Asus' ");
-
-        Query query = session.createQuery(
-                "select brand, model from Laptop where brand like :brand",
-                Object[].class
-        );
-        query.setParameter("brand", brand);
-        List<Object[]> list = query.getResultList();
-
-
-//        List<Laptop> laptops = query.getResultList();
-        List<Object[]> laptops = query.getResultList();
-
-        for (Object[] data : laptops) {
-            System.out.println((String) data[0] + " : " + (String) data[1]);
-        }
-//        Laptop l1=session.get(Laptop.class, 3);
-
-
-        System.out.println(laptops);
-
+        Laptop laptop = session.getReference(Laptop.class,2);
+        System.out.println(laptop);
         session.close();
+
 
         sf.close();
 
