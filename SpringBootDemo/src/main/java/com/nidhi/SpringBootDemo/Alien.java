4b40e595d802b74205733cb18f0f6052f1e03b96
Nidhi <ojhanidhi036@gmail.com>
2025-11-10 11:28:22 +0530

    DI using springboot

---
+++ SpringBootDemo/src/main/java/com/nidhi/SpringBootDemo/Alien.java
@@ -0,0 +1,13 @@
+package com.nidhi.SpringBootDemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+
+    public void code()
+    {
+        System.out.println("Coding");
+    }
+
+}
---
+++ SpringBootDemo/src/main/java/com/nidhi/SpringBootDemo/SpringBootDemoApplication.java
@@ -0,0 +1,18 @@
+package com.nidhi.SpringBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
+
+//		Alien obj = new Alien();
+		Alien obj = context.getBean(Alien.class);
+		obj.code();
+	}
+
+}
