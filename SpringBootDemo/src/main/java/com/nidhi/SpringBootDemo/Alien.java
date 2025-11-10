acc5084d9a5e2dfc47ddd9b469a64d61e71c55fe
Nidhi <ojhanidhi036@gmail.com>
2025-11-10 12:00:24 +0530

    autowiring

--- SpringBootDemo/src/main/java/com/nidhi/SpringBootDemo/Alien.java
+++ SpringBootDemo/src/main/java/com/nidhi/SpringBootDemo/Alien.java
@@ -1,13 +1,17 @@
 package com.nidhi.SpringBootDemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
 
+    @Autowired
+    Laptop laptop;
+
     public void code()
     {
-        System.out.println("Coding");
+        laptop.compile();
     }
 
 }
