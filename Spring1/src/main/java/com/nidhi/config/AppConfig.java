22fe18dfa9e78dc812af9dcb8b1939a1ce515513
Nidhi <ojhanidhi036@gmail.com>
2025-11-15 23:11:32 +0530

    @Primary

--- Spring1/src/main/java/com/nidhi/config/AppConfig.java
+++ Spring1/src/main/java/com/nidhi/config/AppConfig.java
@@ -7,13 +7,14 @@ import com.nidhi.Laptop;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public Alien alien(@Qualifier("desktop") Computer com)
+    public Alien alien(Computer com)
     {
         Alien obj = new Alien();
         obj.setAge(25);
@@ -30,6 +31,7 @@ public class AppConfig {
     }
 
     @Bean
+    @Primary
     public Laptop laptop(){
         return  new Laptop();
     }
