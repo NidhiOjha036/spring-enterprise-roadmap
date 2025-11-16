ec1150d6430e30df214b26c7eeaa73a2b5478b82
Nidhi <ojhanidhi036@gmail.com>
2025-11-16 22:06:12 +0530

    added interface(Computer),@Primary annotation

--- SpringBootDemo/src/main/java/com/nidhi/SpringBootDemo/Alien.java
+++ SpringBootDemo/src/main/java/com/nidhi/SpringBootDemo/Alien.java
@@ -1,17 +1,37 @@
 package com.nidhi.SpringBootDemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
 
+    @Value("25")
+    private int age;
+
+    private Computer com;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Computer getCom() {
+        return com;
+    }
+
     @Autowired
-    Laptop laptop;
+    public void setCom(Computer com) {
+        this.com = com;
+    }
 
     public void code()
     {
-        laptop.compile();
+        com.compile();
     }
 
 }
---
+++ SpringBootDemo/src/main/java/com/nidhi/SpringBootDemo/Computer.java
@@ -0,0 +1,5 @@
+package com.nidhi.SpringBootDemo;
+
+public interface Computer {
+    void compile();
+}
