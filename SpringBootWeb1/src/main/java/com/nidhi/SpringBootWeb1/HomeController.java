4a0a119a440aeff040bb152a374bb4fe3f56961a
Nidhi <ojhanidhi036@gmail.com>
2025-11-23 18:20:00 +0530

    Request param

--- SpringBootWeb1/src/main/java/com/nidhi/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/nidhi/SpringBootWeb1/HomeController.java
@@ -1,9 +1,9 @@
 package com.nidhi.SpringBootWeb1;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -16,10 +16,8 @@ public class HomeController {
     }
 
     @RequestMapping(("add"))
-    public String add(HttpServletRequest req, HttpSession session)
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, HttpSession session)
     {
-        int num1 = Integer.parseInt(req.getParameter("num1"));
-        int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = num1 + num2;
         session.setAttribute("result",result);
         return "result.jsp";
