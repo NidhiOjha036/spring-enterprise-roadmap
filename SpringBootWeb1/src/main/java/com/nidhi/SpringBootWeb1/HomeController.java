873f9e03a7c49d2497548f95003de5042e6df505
Nidhi <ojhanidhi036@gmail.com>
2025-11-23 15:48:41 +0530

    created jsp and contoller page

---
+++ SpringBootWeb1/src/main/java/com/nidhi/SpringBootWeb1/HomeController.java
@@ -0,0 +1,12 @@
+package com.nidhi.SpringBootWeb1;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+    public String home()
+    {
+        return "index.jsp";
+    }
+}
