829bde28a80cd814492c2b4710814d8bc7fcb3af
Nidhi <ojhanidhi036@gmail.com>
2025-11-24 15:03:09 +0530

    Working with layes(mvc)

--- JobApp/src/main/java/com/nidhi/JobApp/JobController.java
+++ JobApp/src/main/java/com/nidhi/JobApp/JobController.java
@@ -1,12 +1,20 @@
 package com.nidhi.JobApp;
 
 import com.nidhi.JobApp.model.JobPost;
+import com.nidhi.JobApp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class JobController {
+
+    @Autowired
+    private JobService service;
 
     @GetMapping({"/", "home"})
     public String home()
@@ -23,7 +31,15 @@ public class JobController {
     @PostMapping("handleForm")
     public String handleForm(JobPost jobPost)
     {
+        service.addJob(jobPost);
         return "success";
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewJobs(Model m){
+        List<JobPost> jobs = service.getAllJobs();
+        m.addAttribute("jobPosts",jobs);
+        return "viewalljobs";
     }
 
 }
