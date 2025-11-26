2c401572f7fe5852cbfc0e7b57b0f2bc52a10ec8
Nidhi <ojhanidhi036@gmail.com>
2025-11-26 13:50:34 +0530

    findAll()

--- spring-data-jpa-ex/src/main/java/com/nidhi/spring_data_jpa_ex/SpringDataJpaExApplication.java
+++ spring-data-jpa-ex/src/main/java/com/nidhi/spring_data_jpa_ex/SpringDataJpaExApplication.java
@@ -18,20 +18,22 @@ public class SpringDataJpaExApplication {
 		Student s2 = context.getBean(Student.class);
 		Student s3 = context.getBean(Student.class);
 
-		s1.setRollNo(101);
-		s1.setName("Navin");
-		s1.setMarks(75);
+//		s1.setRollNo(101);
+//		s1.setName("Navin");
+//		s1.setMarks(75);
+//
+//		s2.setRollNo(102);
+//		s2.setName("Kiran");
+//		s2.setMarks(80);
+//
+//		s3.setRollNo(103);
+//		s3.setName("Harsh");
+//		s3.setMarks(70);
+//
+//		repo.save(s2);
+//		repo.save(s3);
 
-		s2.setRollNo(102);
-		s2.setName("Kiran");
-		s2.setMarks(80);
-
-		s3.setRollNo(103);
-		s3.setName("Harsh");
-		s3.setMarks(70);
-
-		repo.save(s2);
-		repo.save(s3);
+		System.out.println(repo.findAll());
 
 	}
 
