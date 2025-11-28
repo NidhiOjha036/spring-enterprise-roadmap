6effebb7d0ee4c210a73bfc41dc24ec4eb7e2bd9
Nidhi <ojhanidhi036@gmail.com>
2025-11-28 18:34:15 +0530

    @After

--- spring-boot-rest/src/main/java/com/nidhi/spring_boot_rest/aop/LoggingAspect.java
+++ spring-boot-rest/src/main/java/com/nidhi/spring_boot_rest/aop/LoggingAspect.java
@@ -1,8 +1,7 @@
 package com.nidhi.spring_boot_rest.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -15,9 +14,29 @@ public class LoggingAspect {
 
     //return type, classname, method name(args)
 
-    @Before("execution(* com.nidhi.spring_boot_rest.service.JobService.getJob(..))")
+    @Before("execution (* com.telusko.springbootrest.service.JobService.getJob(..)) || execution(* com.telusko.springbootrest.service.JobService.updateJob(..))")
     public void logMethodCall(JoinPoint jp) {
         LOGGER.info("Method Called "+jp.getSignature().getName());
+    }
+
+
+
+    @After("execution (* com.telusko.springbootrest.service.JobService.getJob(..)) || execution(* com.telusko.springbootrest.service.JobService.updateJob(..))")
+    public void logMethodExecuted(JoinPoint jp) {
+        LOGGER.info("Method Executed "+jp.getSignature().getName());
+    }
+
+
+    @AfterThrowing("execution (* com.telusko.springbootrest.service.JobService.getJob(..)) || execution(* com.telusko.springbootrest.service.JobService.updateJob(..))")
+    public void logMethodCrashed(JoinPoint jp) {
+        LOGGER.info("Method has some issues "+jp.getSignature().getName());
+    }
+
+
+
+    @AfterReturning("execution (* com.telusko.springbootrest.service.JobService.getJob(..)) || execution(* com.telusko.springbootrest.service.JobService.updateJob(..))")
+    public void logMethodExecutedSuccess(JoinPoint jp) {
+        LOGGER.info("Method Executed Successfully "+jp.getSignature().getName());
     }
 
 }
