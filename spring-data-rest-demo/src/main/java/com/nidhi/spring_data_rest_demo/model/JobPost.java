6750aa8edb1487e2a69884251798b3f20d881fb2
Nidhi <ojhanidhi036@gmail.com>
2025-11-28 15:28:40 +0530

    Spring Data Rest

---
+++ spring-data-rest-demo/src/main/java/com/nidhi/spring_data_rest_demo/model/JobPost.java
@@ -0,0 +1,27 @@
+package com.nidhi.spring_data_rest_demo.model;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class JobPost {
+
+    @Id
+    private int postId;
+    private String postProfile;
+    private String postDesc;
+    private Integer reqExperience;
+    private List<String> postTechStack;
+
+
+}
