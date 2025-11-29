e4bc62d1625d1a6e2ca7dc24e90fa57c081d0dce
Nidhi <ojhanidhi036@gmail.com>
2025-11-29 22:22:26 +0530

    default login form

---
+++ spring-sec-demo/src/test/java/com/nidhi/spring_sec_demo/SpringSecDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nidhi.spring_sec_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
