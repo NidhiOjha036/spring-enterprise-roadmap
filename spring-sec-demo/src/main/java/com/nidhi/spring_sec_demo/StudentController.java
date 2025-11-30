9f9d9b89a3c863cdb657e5914b8f10734f1d38d1
Nidhi <ojhanidhi036@gmail.com>
2025-11-30 10:13:20 +0530

    sending csrf token

--- spring-sec-demo/src/main/java/com/nidhi/spring_sec_demo/StudentController.java
+++ spring-sec-demo/src/main/java/com/nidhi/spring_sec_demo/StudentController.java
@@ -1,5 +1,7 @@
 package com.nidhi.spring_sec_demo;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +17,11 @@ public class StudentController {
             new Student(1,"Navin","Java"),
             new Student(2,"Kiran","Blockchain")
     ));
+
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
 
     @GetMapping("students")
     public List<Student> getStudents(){
