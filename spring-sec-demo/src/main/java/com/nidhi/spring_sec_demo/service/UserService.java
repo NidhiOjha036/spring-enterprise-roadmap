21eb74f84c7138ae513f12fb0b9d4536ec3c995f
Nidhi <ojhanidhi036@gmail.com>
2025-12-01 15:54:14 +0530

    BCrypt Encoding for User Registration

--- spring-sec-demo/src/main/java/com/nidhi/spring_sec_demo/service/UserService.java
+++ spring-sec-demo/src/main/java/com/nidhi/spring_sec_demo/service/UserService.java
@@ -3,14 +3,20 @@ package com.nidhi.spring_sec_demo.service;
 import com.nidhi.spring_sec_demo.dao.UserRepo;
 import com.nidhi.spring_sec_demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
     @Autowired
     private UserRepo repo;
 
+    private BCryptPasswordEncoder encoder=new BCryptPasswordEncoder(12);
+
     public User saveUser(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        System.out.println(user.getPassword());
         return repo.save(user) ;
 
     }
