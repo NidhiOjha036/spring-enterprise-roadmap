9b483d57ebda384077c987b80962c9ec6784b014
Nidhi <ojhanidhi036@gmail.com>
2025-12-01 15:16:17 +0530

    user registration

---
+++ spring-sec-demo/src/main/java/com/nidhi/spring_sec_demo/service/UserService.java
@@ -0,0 +1,17 @@
+package com.nidhi.spring_sec_demo.service;
+
+import com.nidhi.spring_sec_demo.dao.UserRepo;
+import com.nidhi.spring_sec_demo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepo repo;
+
+    public User saveUser(User user) {
+        return repo.save(user) ;
+
+    }
+}
