985bef10b0fd83ff3e4c196aff25dd73a718ff14
Nidhi <ojhanidhi036@gmail.com>
2025-12-03 10:49:15 +0530

    Custom Login

--- spring-sec-demo/src/main/java/com/nidhi/spring_sec_demo/controller/UserController.java
+++ spring-sec-demo/src/main/java/com/nidhi/spring_sec_demo/controller/UserController.java
@@ -3,6 +3,9 @@ package com.nidhi.spring_sec_demo.controller;
 import com.nidhi.spring_sec_demo.model.User;
 import com.nidhi.spring_sec_demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +16,24 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
     @PostMapping("register")
     public User register(@RequestBody User user) {
         return service.saveUser(user);
     }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user)
+    {
+
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if(authentication.isAuthenticated())
+            return "Success";
+        else
+            return "Login Failed";
+    }
+
 }
--- spring-sec-demo/src/main/java/com/nidhi/spring_sec_demo/model/User.java
+++ spring-sec-demo/src/main/java/com/nidhi/spring_sec_demo/model/User.java
@@ -1,8 +1,6 @@
 package com.nidhi.spring_sec_demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +12,7 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String password;
