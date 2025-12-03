0e99e35bff3aeb854c321fa1babeedb08dd0dbef
Nidhi <ojhanidhi036@gmail.com>
2025-12-03 13:34:48 +0530

    jwt filter created and setting authtoken in securityContext

--- spring-sec-demo/src/main/java/com/nidhi/spring_sec_demo/service/JwtService.java
+++ spring-sec-demo/src/main/java/com/nidhi/spring_sec_demo/service/JwtService.java
@@ -1,9 +1,11 @@
 package com.nidhi.spring_sec_demo.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
@@ -14,6 +16,9 @@ import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+
+import static io.jsonwebtoken.Jwts.*;
 
 
 @Service
@@ -41,7 +46,7 @@ public class JwtService {
     public String generateToken(String username) {
         Map<String, Object> claims = new HashMap<>(); // Claims can include custom data (e.g., roles, permissions)
         claims.put("username", username); // Adding custom claim
-        return Jwts.builder()
+        return builder()
                 .setClaims(claims) // Add claims to the token
                 .setSubject(username) // Set the subject (e.g., the username)
                 .setIssuedAt(new Date(System.currentTimeMillis())) // Current time as issue time
@@ -55,5 +60,37 @@ public class JwtService {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
+
+    public String extractUserName(String token) {
+        // extract the username from jwt token
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build().parseClaimsJws(token).getBody();
+    }
+
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String userName = extractUserName(token);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
 
 }
